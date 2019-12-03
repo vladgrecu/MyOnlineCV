2e9ac20808ceeee9bf873be4221b7d6ee2585d20
vladgrecu <51508637+vladgrecu@users.noreply.github.com>
2019-12-03 23:34:14 +0200

    initial

--- src/components/Resume/Arrow.jsx
+++ src/components/Resume/Arrow.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Arrow({ className }) {
   return (
     <div className={className}>
-      <i className="fas fa-chevron-circle-left fa-2x"></i>
+      <i className="fas fa-chevron-circle-left fa-lg"></i>
     </div>
   );
 }
